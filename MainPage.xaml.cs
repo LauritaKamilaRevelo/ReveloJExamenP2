14b90d49ccf5e548684c9e88b28767eb1a11f92d
LauritaKamilaRevelo <laurita.revelo@udla.edu.ec>
2024-12-02 10:45:22 -0500

    MainPage.xaml.cs, MainPage.xaml, codigo boton convertir

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,22 +8,43 @@ namespace ReveloJExamenP2
             InitializeComponent();
         }
 
-        private double ConvertVolume(double value, string fromUnit, string toUnit)
+        private double ConvertVolume(double valor, string fromUnit, string toUnit)
         {
             double valorEnLitros = fromUnit switch
             {
-                "Litros" => value,
-                "Galones" => value * 3.78541,
-                "Metros Cúbicos" => value * 1000,
-                _ => value
+                "Litros" => valor,
+                "Galones" => valor * 3.78541,
+                "Metros Cubicos" => valor * 1000,
+                _ => valor
             };
             return toUnit switch
             {
                 "Litros" => valorEnLitros,
                 "Galones" => valorEnLitros / 3.78541,
-                "Metros Cúbicos" => valorEnLitros / 1000,
+                "Metros CUbicos" => valorEnLitros / 1000,
                 _ => valorEnLitros
             };
+        }
+        private void KROnConvertButtonClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(KRValueEntry.Text, out double valor))
+            {
+                string fromUnit = KRFromUnitPicker.SelectedItem?.ToString();
+                string toUnit = KRToUnitPicker.SelectedItem?.ToString();
+                if (!string.IsNullOrEmpty(fromUnit) && !string.IsNullOrEmpty(toUnit))
+                {
+                    double result = ConvertVolume(valor, fromUnit, toUnit);
+                    KRResultLabel.Text = $"{valor} {fromUnit} = {result:F2} {toUnit}";
+                }
+                else
+                {
+                    KRResultLabel.Text = "Selecciona unidades validas";
+                }
+            }
+            else
+            {
+                KRResultLabel.Text = "Por favor, introduce un valor valido";
+            }
         }
     }
 }
